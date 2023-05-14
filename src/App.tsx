25a9409e6506d8bfd012ea5eefaaf5849c205fbd
es39 <esj10025@gmail.com>
2023-05-14 20:40:17 +0900

    :sparkles: timer add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
-  return <div className="App">timer-project</div>;
-}
+const App = () => {
+  const [time, setTime] = useState(10);
+
+  useEffect(() => {
+    const count = setInterval(() => {
+      // 1초씩 감소
+      setTime((time) => (time -= 1));
+    }, 1000);
+    if (time === 0) clearInterval(count);
+    return () => clearInterval(count);
+  }, [time]);
+
+  return (
+    <>
+      <header>My Timer</header>
+      <div>
+        <button>시작</button>
+        <button>멈춤</button>
+        <button>초기화</button>
+      </div>
+      <div>timer : {time}</div>
+    </>
+  );
+};
 
 export default App;
